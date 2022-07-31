28ed6efc3a4fd7aae62c42e98106c3ff6713886f
Danae Vogiatzi <dvogiatzi@gmail.com>
2022-07-31 20:54:27 +0300

    feat(slider): add slider draft with dummy elements

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-import './style.css'
+const slidesContainer = document.getElementById('slides-container');
+const slide = document.querySelector('.slide');
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const prevButton = document.getElementById('slide-arrow-prev');
+const nextButton = document.getElementById('slide-arrow-next');
+
+nextButton.addEventListener('click', () => {
+  const slideWidth = slide.clientWidth * 3;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener('click', () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
